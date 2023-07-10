f527645e13dc7c90e24f1882f41df3ca55fcae23
Emma Zemler <emma.zemler@nasa.gov>
2023-07-10 17:47:46 -0500

    added 4040 hatch manager

--- hatch_4040/scripts/hatch4040_manager.py
+++ hatch_4040/scripts/hatch4040_manager.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import rclpy
 from rclpy.node import Node
@@ -13,54 +15,20 @@ class Hatch4040Manager(Node):
     def __init__(self):
         super().__init__('hatch4040_manager')
 
-        self.declare_parameter('robot_description', '') # putting None in as a default causes deprecation warning
-        
-        robot_description = self.get_parameter('robot_description').value
-        if not robot_description:
-            self.get_logger().error('Passing in the robot description is required for this node ({name}) to operator'.format(name=self.get_name())) 
-            return
-
-        # initialize the transform broadcaster
-        self._tf_publisher = StaticTransformBroadcaster(self)
-        self.tf_name = 'TODO_INTIAL_FRAME'
-
-        # intialize tf timer and service callback
-        tf_timer_period_sec = 0.5 # unit: seconds
-        self.tf_timer = self.create_timer(tf_timer_period_sec, self.make_transform)
-
-        self.pose_stamped = PoseStamped() # will store timestamped data about the source frame
-        # initialize the header
-        self.pose_stamped.header.stamp = self.get_clock().now().to_msg() # initialize to the current time
-        self.pose_stamped.header.frame_id = 'world' 
-
-        self.pose_stamped.pose.position.x = 0
-        self.pose_stamped.pose.position.y = 0
-        self.pose_stamped.pose.position.z = 0
-
-        self.pose_stamped.pose.orientation.x = 0
-        self.pose_stamped.pose.orientation.y = 0
-        self.pose_stamped.pose.orientation.z = 0
-        self.pose_stamped.pose.orientation.w = 1
-
-        # callback groups
-        self.position_cb_group = MutuallyExclusiveCallbackGroup() # the group for all of the position callbacks, which has them all go one at a time
 
         # create the joint state publisher
-        self.publisher_ = self.create_publisher(JointState, 'hatch4040_joint_states', 10, callback_group = self.pub_cb_group)
+        self.publisher_ = self.create_publisher(JointState, 'joint_states', 10)
         
         # create subscriptions's to revolute/prismatic joints
-        self.hatch_sub = self.create_subscription(Float64, 'hatch_position', self.hatch_pos_cb, 10, callback_group = self.position_cb_group)
-        self.ext_rot_sub = self.create_subscription(Float64, 'ext_wheel_position', self.ext_wheel_pos_cb, 10, callback_group = self.position_cb_group)
-        self.ext_rot_h_sub = self.create_subscription(Float64, 'ext_handle_position', self.ext_handle_pos_cb, 10, callback_group = self.position_cb_group)
-        self.int_rot_sub = self.create_subscription(Float64, 'int_wheel_position', self.int_wheel_pos_cb, 10, callback_group = self.position_cb_group)
-        self.int_rot_h_sub = self.create_subscription(Float64, 'int_handle_position', self.int_handle_pos_cb, 10, callback_group = self.position_cb_group)
-
-        # initialize the thread lock
-        self.lock = threading.Lock() # the lock is used to freeze data inputs while the joint state pubisher callback is operating
+        self.hatch_sub = self.create_subscription(Float64, 'hatch_position', self.hatch_pos_cb, 10)
+        self.ext_rot_sub = self.create_subscription(Float64, 'ext_wheel_position', self.ext_wheel_pos_cb, 10)
+        self.ext_rot_h_sub = self.create_subscription(Float64, 'ext_handle_position', self.ext_handle_pos_cb, 10)
+        self.int_rot_sub = self.create_subscription(Float64, 'int_wheel_position', self.int_wheel_pos_cb, 10)
+        self.int_rot_h_sub = self.create_subscription(Float64, 'int_handle_position', self.int_handle_pos_cb, 10)
 
         # create the timer for joint state publisher callback
         timer_period_sec = 0.5 # unit: seconds
-        self.timer = self.create_timer(timer_period_sec, self.hatch4040_joint_state_cb, callback_group = self.pub_cb_group)
+        self.timer = self.create_timer(timer_period_sec, self.hatch4040_joint_state_cb)
 
         # initialization of all the latch component positions
         self.hatch_position = 0.0
@@ -68,6 +36,8 @@ class Hatch4040Manager(Node):
         self.ext_handle_position = 0.0
         self.int_wheel_position = 0.0
         self.int_handle_position = 0.0
+
+        self.lock = threading.Lock() 
 
         # ranges for all joints
         self.hatch_min = 0
@@ -85,29 +55,9 @@ class Hatch4040Manager(Node):
         self._hatch_joint_states = {'hatch_4040_frame_face_joint': copy.deepcopy(self.hatch_position), 'external_rotary_joint': copy.deepcopy(self.ext_wheel_position), \
             'external_rotary_handle_joint': copy.deepcopy(self.ext_handle_position), 'internal_rotary_joint': copy.deepcopy(self.int_wheel_position), 'internal_rotary_handle_joint': copy.deepcopy(self.int_handle_position)}           
 
-        # go ahead and publish first joint_state. Get TF working (these are just in case since we don't know the executor's timing trigger for these things)
-        self.hatch4040_joint_state_cb()
-        # go ahead and publish first transform
-        self.make_transform()
 
+        self.hatch4040_joint_state_cb() # going ahead and starting
 
-    def make_transform(self):
-        """ function to make the tf transform stamped message and pass it to the broadcaster 
-        """
-        transform = TransformStamped()
-
-        # set the parameters of the transform to those updated globally
-        transform.header.stamp = self.pose_stamped.header.stamp 
-        transform.header.frame_id = self.pose_stamped.header.frame_id 
-        transform.child_frame_id = self.tf_name
-        transform.transform.translation.x = self.pose_stamped.pose.position.x # note: had to break down the xyz because pose stamped uses a Point while transform stamped uses a vector3
-        transform.transform.translation.y = self.pose_stamped.pose.position.y
-        transform.transform.translation.z = self.pose_stamped.pose.position.z
-        transform.transform.rotation = self.pose_stamped.pose.orientation # this is a quaternion
-
-        # publish the transformation to the static transform broadcaster
-        self._tf_publisher.sendTransform(transform)
-       
 
     def hatch4040_joint_state_cb(self):
         """Publisher for the manager which publishes the joint state info. 
@@ -117,8 +67,8 @@ class Hatch4040Manager(Node):
         msg.name = ['hatch_4040_frame_face_joint', 'external_rotary_joint', 'external_rotary_handle_joint', 'internal_rotary_joint', 'internal_rotary_handle_joint']
         with self.lock: 
             msg.position = [self.hatch_position, self.ext_wheel_position, self.ext_handle_position, self.int_wheel_position, self.int_handle_position] # fill with float64 list
-            msg.velocity = [] # fill with float64 list
-            msg.effort = [] # fill with float64 list
+            msg.velocity = [0.0, 0.0, 0.0, 0.0, 0.0]
+            msg.effort = [0.0, 0.0, 0.0, 0.0, 0.0]
             for x, joint_name in enumerate(msg.name):
                 if joint_name in self._hatch_joint_states.keys():
                     self._hatch_joint_states[joint_name] = copy.deepcopy(msg.position[x]) # go ahead and update
@@ -137,6 +87,12 @@ class Hatch4040Manager(Node):
         if self.hatch_min <= msg.data and msg.data  <= self.hatch_max:
             with self.lock: 
                 self.hatch_position = msg.data
+        elif self.hatch_min >= msg.data:
+            with self.lock:
+                self.hatch_position = copy.deepcopy(self.hatch_min)
+        elif self.hatch_max <= msg.data:
+            with self.lock:
+                self.hatch_position = copy.deepcopy(self.hatch_max)
         else:
             raise Exception("hatch joint angle out of range")
     
@@ -150,6 +106,12 @@ class Hatch4040Manager(Node):
         if self.ext_wheel_min <= msg.data and msg.data <= self.ext_wheel_max:
             with self.lock: 
                 self.ext_wheel_position = msg.data
+        elif self.ext_wheel_min >= msg.data:
+            with self.lock:
+                self.ext_wheel_position = copy.deepcopy(self.ext_wheel_min)
+        elif self.ext_wheel_max <= msg.data:
+            with self.lock:
+                self.ext_wheel_position = copy.deepcopy(self.ext_wheel_max)                
         else:
             raise Exception("external wheel value out of range")
     
@@ -163,6 +125,12 @@ class Hatch4040Manager(Node):
         if self.ext_handle_min <= msg.data and msg.data <= self.ext_handle_max:
             with self.lock: 
                 self.ext_handle_position = msg.data
+        elif self.ext_handle_min >= msg.data:
+            with self.lock:
+                self.ext_handle_position = copy.deepcopy(self.ext_handle_min)
+        elif self.ext_handle_max <= msg.data:
+            with self.lock:
+                self.ext_handle_position = copy.deepcopy(self.ext_handle_max)                 
         else:
             raise Exception("external handle value out of range")
 
@@ -173,9 +141,15 @@ class Hatch4040Manager(Node):
             msg (Float64): This message transmits the revolute joint angle (in radians) 
         """
         self.get_logger().debug('This is the angle of the internal wheel joint: {}'.format(msg))
-        if self.int_wheel_min <= msg.data and msg.data <= self.int_wheel_min:
+        if self.int_wheel_min <= msg.data and msg.data <= self.int_wheel_max:
             with self.lock: 
                 self.int_wheel_position = msg.data
+        elif self.int_wheel_min >= msg.data:
+            with self.lock:
+                self.int_wheel_position = copy.deepcopy(self.int_wheel_min)
+        elif self.int_wheel_max <= msg.data:
+            with self.lock:
+                self.int_wheel_position = copy.deepcopy(self.int_wheel_max)                  
         else:
             raise Exception("internal wheel joint angle out of range")
         
@@ -189,5 +163,28 @@ class Hatch4040Manager(Node):
         if self.int_handle_min <= msg.data and msg.data <= self.int_handle_max:
             with self.lock: 
                 self.int_handle_position = msg.data
+        elif self.int_handle_min >= msg.data:
+            with self.lock:
+                self.int_handle_position = copy.deepcopy(self.int_handle_min)
+        elif self.int_handle_max <= msg.data:
+            with self.lock:
+                self.int_handle_position = copy.deepcopy(self.int_handle_max)                     
         else:
             raise Exception("internal handle joint angle out of range")
+        
+def main(args=None):
+    rclpy.init(args=args)
+
+    hatch_manager = Hatch4040Manager()
+
+    rclpy.spin(hatch_manager)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    hatch_manager.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()        
