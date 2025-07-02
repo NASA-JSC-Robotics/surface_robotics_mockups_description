e284f0a7550f617be404ff6eaad9e8cc7aa1ddba
Erik Holum <erik.holum@nasa.gov>
2025-07-02 15:20:03 -0400

    License and formatting

--- launch/view_merlin_mockup.launch.py
+++ launch/view_merlin_mockup.launch.py
@@ -1,9 +1,28 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2025, United States Government, as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+#
+# All rights reserved.
+#
+# This software is licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.conditions import IfCondition, UnlessCondition
+from launch.conditions import IfCondition
 
 def generate_launch_description():
     declared_arguments = []
@@ -29,10 +48,10 @@ def generate_launch_description():
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution([FindPackageShare("merlin_mockup_description"), "urdf", "merlin_mockup.urdf.xacro"]),
-            " ", 
+            " ",
             "example_arg:=",
             example_arg,
-            " "                                        
+            " "
         ]
     )
     robot_description = {"robot_description": robot_description_content}
@@ -45,7 +64,7 @@ def generate_launch_description():
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui",
     )
-   
+
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
